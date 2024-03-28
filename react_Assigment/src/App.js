f2cbef36d614fa6c604b7cf404dbda5ec5b97caf
nileshpanchalnp <nileshpanchal4411@gmail.com>
2024-03-28 15:05:38 +0530

    react assigment

---
+++ react_Assigment/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Component/Header';
+import Sidbar from './Component/Sidbar';
+import Footer from './Component/Footer';
+import First_assigment from './Es5_Es6_array/First_assigment';
+import Comment from './form/Comment'
+import Control from './form/Control'
+import Uncontrol from './form/Uncontrol'
+import ArrayandArrayofObject from './event/ArrayandArrayofObject';
+import Button from './event/Button';
+import Ckeckbox from './event/Ckeckbox';
+import Toggle from './event/Toggle';
+
+
+
+function App() {
+  return (
+    <div >
+      <First_assigment/>
+      {/* <Header/>
+      <Sidbar/>
+      <Footer/>
+      <ArrayandArrayofObject/>
+      <Button/>
+      <Ckeckbox/>
+      <Toggle/>
+      <Comment/>
+      <Control/>
+      <Uncontrol/> */}
+    </div>
+  );
+}
+
+export default App;
